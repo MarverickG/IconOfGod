43f6d1e9f77c4980420793914a8913f0ebb176f9
MarverickG <Marverickgonzales@gmail.com>
2025-04-17 16:00:09 -0400

    added models/user.js

--- IconOfGod_Backend/models/user.js
+++ IconOfGod_Backend/models/user.js
@@ -1,3 +1,11 @@
-// data/users.js
-const users = [];
-module.exports = users;
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  verified: { type: Boolean, default: false },
+  verificationToken: String,
+  resetToken: String,
+});
+
+module.exports = mongoose.model('User', userSchema);
