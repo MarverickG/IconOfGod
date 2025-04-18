1fabe28676caa7bda80fa488e7397d977723b33f
MarverickG <Marverickgonzales@gmail.com>
2025-04-18 14:21:30 -0400

    returened back to launch ready

--- IconOfGod_Backend/routes/cart.js
+++ IconOfGod_Backend/routes/cart.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middleware/authMiddleware');
 const { addToCart, getCart, removeFromCart } = require('../controllers/cartController');
-const authenticateUser = require('../middleware/authenticateUser');
+
 
 // Secure routes using middleware
 router.get('/', verifyToken, getCart);
 router.post('/add', verifyToken, addToCart);
 router.post('/remove', verifyToken, removeFromCart);
-router.delete('/remove/:itemId', authenticateUser, deleteCartItem);
+
 module.exports = router;
