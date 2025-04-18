ab834cc3c92889f80ca413348497e7893ac41441
MarverickG <Marverickgonzales@gmail.com>
2025-04-18 19:37:36 -0400

    edited signup.html frontend

--- IconOfGod_Backend/server.js
+++ IconOfGod_Backend/server.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5500', // frontend origin
+  origin: 'https://iconofgod-frontend.onrender.com', // frontend origin
   credentials: true // allow cookies
 }));
 app.use(express.json());
